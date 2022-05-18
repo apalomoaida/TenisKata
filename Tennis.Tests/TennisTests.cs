379139fa5e8ef8a1171ad6e6b15787f297e323f1
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-05-18 17:43:15 +0200

    extract methods from if

--- Tennis.Tests/TennisTests.cs
+++ Tennis.Tests/TennisTests.cs
@@ -51,27 +51,35 @@ namespace Tennis.Tests
 
     public class TennisTests
     {
-        [Theory]
-        [ClassData(typeof(TestDataGenerator))]
-        public void Tennis1Test(int p1, int p2, string expected)
-        {
-            var game = new TennisGame1("player1", "player2");
-            CheckAllScores(game, p1, p2, expected);
-        }
+        //[Theory]
+        //[ClassData(typeof(TestDataGenerator))]
+        //public void Tennis1Test(int p1, int p2, string expected)
+        //{
+        //    var game = new TennisGame1("player1", "player2");
+        //    CheckAllScores(game, p1, p2, expected);
+        //}
 
-        [Theory]
-        [ClassData(typeof(TestDataGenerator))]
-        public void Tennis2Test(int p1, int p2, string expected)
-        {
-            var game = new TennisGame2("player1", "player2");
-            CheckAllScores(game, p1, p2, expected);
-        }
+        //[Theory]
+        //[ClassData(typeof(TestDataGenerator))]
+        //public void Tennis2Test(int p1, int p2, string expected)
+        //{
+        //    var game = new TennisGame2("player1", "player2");
+        //    CheckAllScores(game, p1, p2, expected);
+        //}
 
+        //[Theory]
+        //[ClassData(typeof(TestDataGenerator))]
+        //public void Tennis3Test(int p1, int p2, string expected)
+        //{
+        //    var game = new TennisGame3("player1", "player2");
+        //    CheckAllScores(game, p1, p2, expected);
+        //}     
+        
         [Theory]
         [ClassData(typeof(TestDataGenerator))]
-        public void Tennis3Test(int p1, int p2, string expected)
+        public void TennisMineTest(int p1, int p2, string expected)
         {
-            var game = new TennisGame3("player1", "player2");
+            var game = new TennisGameMine("player1", "player2");
             CheckAllScores(game, p1, p2, expected);
         }
 
--- Tennis/TennisGameMine.cs
+++ Tennis/TennisGameMine.cs
@@ -2,29 +2,34 @@ namespace Tennis {
 
     public class TennisGameMine : ITennisGame
     {
-        private readonly PlayersScores _playersScores;
+        private static PlayersScores _playersScores;
 
         public TennisGameMine(string player1Name, string player2Name)
         {
             _playersScores = new PlayersScores(player1Name, player2Name);
         }
 
-        public string GetScore()
-        {
+        public string GetScore() {
             string s;
-            if ((_playersScores.Player1Score < 4 && _playersScores.Player2Score < 4) && (_playersScores.Player1Score + _playersScores.Player2Score < 6))
-            {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[_playersScores.Player1Score];
-                return (_playersScores.Player1Score == _playersScores.Player2Score) ? s + "-All" : s + "-" + p[_playersScores.Player2Score];
+            if (ScoreMinorThan4() && (SumScoreMinorThan6())) {
+                string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
+                s = scoreNames[_playersScores.Player1Score];
+                return (_playersScores.Player1Score == _playersScores.Player2Score) ? s + "-All" : s + "-" + scoreNames[_playersScores.Player2Score];
             }
-            else
-            {
+            else {
                 if (_playersScores.Player1Score == _playersScores.Player2Score)
                     return "Deuce";
                 s = _playersScores.Player1Score > _playersScores.Player2Score ? _playersScores.Player1Name : _playersScores.Player2Name;
                 return ((_playersScores.Player1Score - _playersScores.Player2Score) * (_playersScores.Player1Score - _playersScores.Player2Score) == 1) ? "Advantage " + s : "Win for " + s;
             }
+        }
+
+        private static bool SumScoreMinorThan6() {
+            return _playersScores.Player1Score + _playersScores.Player2Score < 6;
+        }
+
+        private static bool ScoreMinorThan4() {
+            return (_playersScores.Player1Score < 4 && _playersScores.Player2Score < 4);
         }
 
         public void WonPoint(string playerName)
