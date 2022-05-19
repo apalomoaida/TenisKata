f166422bb3902cec4cdca24e117d4c5300cda594
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-05-19 19:25:21 +0200

    change method visibility

--- Tennis/ScoreService.cs
+++ Tennis/ScoreService.cs
@@ -23,11 +23,11 @@
             return false;
         }
 
-        public bool SumScoreMinorThan6() {
+        private bool SumScoreMinorThan6() {
             return _playersScores.Player1Score + _playersScores.Player2Score < 6;
         }
 
-        public bool ScoreMinorThan4() {
+        private bool ScoreMinorThan4() {
             return (_playersScores.Player1Score < 4 && _playersScores.Player2Score < 4);
         }
 
